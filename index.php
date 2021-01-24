8fb5c25ee6b78b938e2dd42e2afe69a7127c2bef
Nux007 <007.nux@gmail.com>
2021-01-24 15:30:53 +0100

    Exo 164.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * A l'aide de la documentation officielle de PHP.
+ * 1. Retrouvez la page traitant des superglobales pour $_SERVER
+ * 2. Récupérez un maximum de valeurs de ce tableau et affichez les à l'aide d'un simple echo ( il n'est pas interdit d'être intelligent, c'est un tableau associatif... )
+ * 3. Uploadez ce projet sur votre serveur et testez !
+ */
+// TODO Votre code ici
